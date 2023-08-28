5ed07f2bcf23b9897334e705330252adcdfb1d6b
efe <eefee1987@gmail.com>
2023-08-28 07:36:21 +0200

    agregado config eslint

--- app/page.js
+++ app/page.js
@@ -1,9 +1,7 @@
-import Image from 'next/image';
-
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1>Reading List</h1>
+    <main>
+      <main>Reading List</main>
     </main>
   );
 }
