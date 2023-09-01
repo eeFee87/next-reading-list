5c09cd284642219e18bb3a5becf853d9308e715b
efe <eefee1987@gmail.com>
2023-09-01 07:39:24 +0200

    add filterbooks

--- app/page.js
+++ app/page.js
@@ -1,23 +1,49 @@
 'use client';
+import { useState } from 'react';
 import data from '../books.json';
 
-const books = data.library.map((data) => data.book);
+const books = data.library.map((item) => item.book);
+const genres = Array.from(new Set(books.map((book) => book.genre)));
+
 export default function Home() {
+  const [genre, setGenre] = useState('');
+  const matches = genre
+    ? books.filter((book) => {
+        if (book.genre !== genre) return false;
+        return true;
+      })
+    : books;
   return (
-    <main>
-      <h1>Reading List</h1>
-      <article className='grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4'>
-        {books.map((book) => (
-          <div key={book.ISBN}>
+    <article className='grid gap-4'>
+      <nav>
+        <select
+          value={genre}
+          onChange={(e) => setGenre(e.target.value)}
+        >
+          <option value=''>Todos</option>
+          {genres.map((genre) => (
+            <option
+              key={genre}
+              value={genre}
+            >
+              {genre}
+            </option>
+          ))}
+        </select>
+      </nav>
+
+      <ul className='grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4'>
+        {matches.map((book) => (
+          <li key={book.ISBN}>
             <img
               className='aspect-[9/14] object-cover'
               src={book.cover}
               alt={book.title}
             />
             <p>{book.title}</p>
-          </div>
+          </li>
         ))}
-      </article>
-    </main>
+      </ul>
+    </article>
   );
 }
